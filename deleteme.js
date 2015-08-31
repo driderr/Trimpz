2c5b2a23cd577fe69a01513c42258e404a1f5415
Batman <Batman@f0b174e5-862d-8b48-a571-97ba3304275b>
2015-08-31 04:35:56 +0000

    Implemented new fancy maps.  Untested.

--- deleteme.js
+++ deleteme.js
@@ -1,4 +1,4 @@
-/*global game,tooltip,resolvePow,getNextPrestigeCost,adjustMap,updateMapCost,addSpecials,debug*/
+/*global game,tooltip,resolvePow,getNextPrestigeCost,adjustMap,updateMapCost,addSpecials*/
 /*jslint plusplus: true */
 var openTrapsForDefault;    //Open traps as default action?
 var trimpz = 0;             //"Trimpz" running indicator
@@ -7,6 +7,8 @@ var workersFocused = false;
 var workersFocusedOn;
 var workersMoved = [];
 var skipShieldBlock = true;
+var mapsWithDesiredUniqueDrops = [8,10,14,15,18,23,25,29,30,34,40,47,50];
+var uniqueMaps = ["The Block", "The Wall",  "Dimension of Anger", "Trimple Of Doom"];
 var constantsEarlyGame = (function () {
     "use strict";
     var zoneToStartAt = 0,
@@ -835,7 +837,7 @@ function GotoMapsScreen() {
     }
 }
 
-function RunNewMap() {
+function RunNewMap(zoneToCreate) {
     "use strict";
     var newMap;
     var size = 9;
@@ -846,6 +848,9 @@ function RunNewMap() {
     adjustMap('difficulty', difficulty);
     document.getElementById("sizeAdvMapsRange").value = size;
     adjustMap('size', size);
+    if (typeof zoneToCreate !== 'undefined') {
+        document.getElementById("mapLevelInput").value = zoneToCreate;
+    }
     var cost = updateMapCost(true);
     while (cost > game.resources.fragments.owned){
         if (size === 1){
@@ -882,24 +887,73 @@ function RunWorld() {
 function RunMaps() {
     "use strict";
     var map;
+    var theMap;
     var itemsAvailable;
+
     if (game.global.world < 7 || game.global.mapsActive === true){ //no map ability(wait one) or already running a map(repeat should be off)
         return;
     }
-    var itemsAvailableInNewMap = addSpecials(true,true,{ id: "map999", name: "My Map", location: "Sea", clears: 0, level: game.global.world, difficulty: 1.11, size: 40, loot: 1.2, noRecycle: false });
-    for (map in game.global.mapsOwnedArray){
-        itemsAvailable = addSpecials(true,true,game.global.mapsOwnedArray[map]);
-        if (itemsAvailable >= itemsAvailableInNewMap && itemsAvailable > 0) {
-            RunMap(game.global.mapsOwnedArray[map]);
-            return;
+
+    if (game.upgrades.Bounty.done === 0 && game.upgrades.Bounty.locked === 1) { //Look for Bounty upgrade
+        for (map in game.global.mapsOwnedArray) {
+            theMap = game.global.mapsOwnedArray[map];
+            if (theMap.name === "The Wall" && addSpecials(true, true, theMap) > 0){
+                RunMap(theMap);
+                return;
+            }
         }
     }
-    if (itemsAvailableInNewMap > 0 && constants.getShouldRunMaps() === true){
+
+    var itemsAvailableInNewMap = addSpecials(true,true,{ id: "map999", name: "My Map", location: "Sea", clears: 0, level: game.global.world, difficulty: 1.11, size: 40, loot: 1.2, noRecycle: false });
+    if (game.global.preMapsActive === true && itemsAvailableInNewMap === 0){
+        RunWorld();
+        return;
+    }
+
+    var uniqueMapIndex = mapsWithDesiredUniqueDrops.indexOf(game.global.world); //Run new map if on zone with unique map drop then remove
+    if (uniqueMapIndex > -1 && itemsAvailableInNewMap > 0){
+        mapsWithDesiredUniqueDrops.splice(uniqueMapIndex,1);
         RunNewMap();
         return;
     }
-    if (game.global.preMapsActive === true){
-        RunWorld();
+
+    //Any equipment upgrades available?
+    var upgrade;
+    var currentUpgrade;
+    var currentEquip;
+    var totalUpgrades = 0;
+    for (upgrade in game.upgrades) {
+        currentUpgrade = game.upgrades[upgrade];
+        currentEquip = game.equipment[game.upgrades[upgrade].prestiges];
+        if (typeof currentUpgrade.prestiges !== 'undefined' && currentUpgrade.locked === 0 && upgrade !== "Supershield") {
+            if (constants.getShouldSkipHpEquipment() === true && typeof currentEquip.health !== 'undefined') { //don't buy hp equips in late late game
+                continue;
+            }
+            totalUpgrades++;
+        }
+    }
+    if (totalUpgrades === 0){ //if not equipment upgrades, go get some! (can make this a "< constant" later if desired)
+        for (map in game.global.mapsOwnedArray){ //look for an existing map first
+            theMap = game.global.mapsOwnedArray[map];
+            if (uniqueMaps.indexOf(theMap.name) > -1){
+                continue;
+            }
+            itemsAvailable = addSpecials(true,true,game.global.mapsOwnedArray[map]);
+            if (itemsAvailable > 0) {
+                RunMap(game.global.mapsOwnedArray[map]);
+                return;
+            }
+        }
+        //what's the lowest zone map I can create and get items?
+        var zoneToTry;
+        for (zoneToTry = 6; zoneToTry <= game.global.world; zoneToTry++){
+            itemsAvailableInNewMap = addSpecials(true,true,{ id: "map999", name: "My Map", location: "Sea", clears: 0, level: zoneToTry, difficulty: 1.11, size: 40, loot: 1.2, noRecycle: false });
+            if (itemsAvailableInNewMap > 0)
+            {
+                RunNewMap(zoneToTry);
+                return;
+            }
+        }
     }
 }
 
